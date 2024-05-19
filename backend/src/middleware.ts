a54433f8a1f3ee4a6aa97d87415a6be3fdefe051
Harsh Keshri <harshkeshri1234567@gmail.com>
2024-05-19 15:47:35 +0530

    Revert "Updated the Auth Logic"
    This reverts commit 8fdd7ab01fd71c34a82db0c74439f3446bd6936e.

--- backend/src/middleware.ts
+++ backend/src/middleware.ts
@@ -8,7 +8,8 @@ export const authMiddleware = async (
 ) => {
   try {
     const token = req.headers.authorization?.split(' ')[1] ?? ''
-    const decoded = jwt.verify(token, process.env.JWT_SECRET!)
+    const JWT_SECRET = process.env.JWT_SECRET ?? ''
+    const decoded = jwt.verify(token, JWT_SECRET)
     res.locals.user = decoded ?? null
   } catch (e) {
     console.log(e)
