a6723f88cd721099645f2964e2dc2d70fd820746
Harsh Keshri <harshkeshri1234567@gmail.com>
2024-05-19 11:49:37 +0530

    For env

--- frontend/utils/config/index.ts
+++ frontend/utils/config/index.ts
@@ -1,6 +1,6 @@
-export const RPC_URL = process.env.RPC_URL ?? 'https://api.devnet.solana.com'
-export const BACKEND_URL = process.env.BACKEND_URL ?? 'http://localhost:8000'
-export const NETWORK = process.env.NETWORK ?? 'devnet'
+export const RPC_URL = process.env.RPC_URL || 'https://api.devnet.solana.com'
+export const BACKEND_URL = process.env.BACKEND_URL || 'http://localhost:8000'
+export const NETWORK = process.env.NETWORK || 'devnet'
 export const BASE_LAMPORTS = process.env.BASE_LAMPORTS
   ? parseInt(process.env.BASE_LAMPORTS)
   : 1000000000
