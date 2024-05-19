8fdd7ab01fd71c34a82db0c74439f3446bd6936e
Harsh Keshri <harshkeshri1234567@gmail.com>
2024-05-19 14:49:27 +0530

    Updated the Auth Logic

--- backend/src/middleware.ts
+++ backend/src/middleware.ts
@@ -8,8 +8,7 @@ export const authMiddleware = async (
 ) => {
   try {
     const token = req.headers.authorization?.split(' ')[1] ?? ''
-    const JWT_SECRET = process.env.JWT_SECRET ?? ''
-    const decoded = jwt.verify(token, JWT_SECRET)
+    const decoded = jwt.verify(token, process.env.JWT_SECRET!)
     res.locals.user = decoded ?? null
   } catch (e) {
     console.log(e)
