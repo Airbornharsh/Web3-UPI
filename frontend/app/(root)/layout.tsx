0b732549588810d99139faa59a835ebe92946446
Harsh Keshri <harshkeshri1234567@gmail.com>
2024-05-19 15:47:14 +0530

    Revert "Updated the Auth Logic"
    This reverts commit d3d775d0d597f9961e576c40db5e2226b69988b1.

--- frontend/app/(root)/layout.tsx
+++ frontend/app/(root)/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({
           <LoaderProvider>
             <AuthProvider>
               <Appbar />
-              {children}
+              <Children>{children}</Children>
               <Loader />
             </AuthProvider>
           </LoaderProvider>
