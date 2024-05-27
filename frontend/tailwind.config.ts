e198c355db4206ff1b0ce726e50b1002f21726ef
Harsh Keshri <harshkeshri1234567@gmail.com>
2024-05-27 23:53:08 +0530

    Added Primary Color to tailwind for new design

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -14,6 +14,9 @@ const config: Config = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
+    colors: {
+      primary: '#19B784',
+    },
   },
   plugins: [],
 }
