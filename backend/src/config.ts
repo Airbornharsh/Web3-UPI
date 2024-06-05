d984ad5a8ad058e200570a0ac8599068287a87f5
Harsh Keshri <harshkeshri1234567@gmail.com>
2024-06-05 10:35:34 +0530

    Added schema for deposit and withraw, Vault Address

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -1,3 +1,4 @@
 export const RPC_URL = process.env.RPC_URL || 'https://api.devnet.solana.com'
 export const NETWORK = process.env.NETWORK || 'devnet'
 export const PRIVATE_KEY = process.env.PRIVATE_KEY || ''
+export const VAULT_PRIVATE_KEY = process.env.VAULT_PRIVATE_KEY || ''
