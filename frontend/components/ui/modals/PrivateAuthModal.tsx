1ba7826c119be75ccbd6b057af66f1f3e3f50943
Harsh Keshri <harshkeshri1234567@gmail.com>
2024-06-12 08:36:31 +0530

    Added Close Button in Auth Modal

--- frontend/components/ui/modals/PrivateAuthModal.tsx
+++ frontend/components/ui/modals/PrivateAuthModal.tsx
@@ -25,6 +25,7 @@ const PrivateAuthModal: React.FC<PrivateAuthModalProps> = ({
     encodedPrivateKey,
     decodePrivateKey,
     getPublicKeyFromPrivateKey,
+    disconnectPrivatWallet,
   } = useCustomWallet()
   const { signIn, signUp } = useAuth()
   const [formData, setFormData] = useState<AuthFormData>({
@@ -191,6 +192,23 @@ const PrivateAuthModal: React.FC<PrivateAuthModalProps> = ({
       >
         Submit
       </Button>
+      <Button
+        variant={'outline'}
+        onClick={(e) => {
+          e.preventDefault()
+          setFormData({
+            name: '',
+            walletAddress: '',
+            upiId: '',
+            pin: '',
+            privateKey: '',
+          })
+          disconnectPrivatWallet()
+          setOpenModal(false)
+        }}
+      >
+        Close
+      </Button>
     </div>
   )
 
@@ -222,6 +240,23 @@ const PrivateAuthModal: React.FC<PrivateAuthModalProps> = ({
         type="submit"
       >
         Submit
+      </Button>
+      <Button
+        variant={'outline'}
+        onClick={(e) => {
+          e.preventDefault()
+          setFormData({
+            name: '',
+            walletAddress: '',
+            upiId: '',
+            pin: '',
+            privateKey: '',
+          })
+          disconnectPrivatWallet()
+          setOpenModal(false)
+        }}
+      >
+        Close
       </Button>
     </div>
   )
@@ -271,6 +306,23 @@ const PrivateAuthModal: React.FC<PrivateAuthModalProps> = ({
         type="submit"
       >
         Submit
+      </Button>
+      <Button
+        variant={'outline'}
+        onClick={(e) => {
+          e.preventDefault()
+          setFormData({
+            name: '',
+            walletAddress: '',
+            upiId: '',
+            pin: '',
+            privateKey: '',
+          })
+          disconnectPrivatWallet()
+          setOpenModal(false)
+        }}
+      >
+        Close
       </Button>
     </div>
   )
